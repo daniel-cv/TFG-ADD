b579bafa717ca97ae9db6f188c8972337f678268
daniel-ch <danielchicharro97@gmail.com>
2025-12-04 20:28:03 +0100

    Commit inicial, creacion del backend

---
+++ SMARTNETWORK/BACKEND/smartnetwork-backend/src/main/java/com/smartnetwork/backend/SmartnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.smartnetwork.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnetworkApplication.class, args);
+	}
+
+}
---
+++ SMARTNETWORK/BACKEND/smartnetwork-backend/src/test/java/com/smartnetwork/backend/SmartnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartnetwork.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
