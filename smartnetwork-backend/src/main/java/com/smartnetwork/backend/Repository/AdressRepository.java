fd245aea27f929a09a9e39c4349834c69b256457
daniel-ch <danielchicharro97@gmail.com>
2026-02-03 20:50:10 +0100

    reestructura de back

--- smartnetwork-backend/src/main/java/com/smartnetwork/backend/Repository/AdressRepository.java
+++ smartnetwork-backend/src/main/java/com/smartnetwork/backend/Repository/AdressRepository.java
@@ -1,7 +1,6 @@
 package com.smartnetwork.backend.Repository;
 
 import com.smartnetwork.backend.domain.Entity.Address;
-import com.smartnetwork.backend.domain.Entity.Configuracion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
