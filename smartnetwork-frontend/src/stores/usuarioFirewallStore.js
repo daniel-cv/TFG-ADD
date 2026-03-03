01c625c9955fdd5c8c373bc8efdd31b226ea1e9d
daniel-ch <danielchicharro97@gmail.com>
2026-03-03 18:21:10 +0100

    Se termina toda la parte de listar

--- smartnetwork-frontend/src/stores/usuarioFirewallStore.js
+++ smartnetwork-frontend/src/stores/usuarioFirewallStore.js
@@ -8,7 +8,7 @@ import {
 
 export const useUsuarioFirewallStore = defineStore('usuarioFirewall', {
   state: () => ({
-    reglas: [],
+    usuarios: [],
     cargando: false,
     mensaje: "",
   }),
